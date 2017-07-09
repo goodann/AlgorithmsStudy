77bbc4836d1baf6ba273a44aece9f75f91875f0e
Sweetio <rlawlstjr4876@gmail.com>
2017-07-09 22:18:49 +0900

    Update 09_Merge two sorted linked lists.cpp

--- JSK/HackerRank/Linked Lists/09_Merge two sorted linked lists.cpp
+++ JSK/HackerRank/Linked Lists/09_Merge two sorted linked lists.cpp
@@ -11,51 +11,49 @@ Node* MergeLists(Node *headA, Node* headB)
 {
   // This is a "method-only" submission. 
   // You only need to complete this method 
-    Node* mergeNodeHead;
-    Node* mergeNode = new Node();
-    mergeNodeHead = mergeNode;
-    Node* cursorMerge=mergeNodeHead;
-    
+    Node* newNode,*newHead;
     Node* cursorA = headA;
     Node* cursorB = headB;
     
-    while ( cursorA != NULL && cursorB != NULL){
+    if( cursorA == NULL || cursorA->data > cursorB->data){
+        newHead=cursorB;
+        newNode=cursorB;
+        cursorB=cursorB->next;
+    }
+    else if ( cursorB == NULL || cursorA->data < cursorB->data ){
+        newHead=cursorA;
+        newNode=cursorA;
+        cursorA=cursorA->next;
+    }
+    
+    while( cursorA != NULL && cursorB != NULL){
         if( cursorA->data < cursorB->data){
-            Node* newNode = new Node();
-            newNode->data= cursorA->data;
-            cursorMerge->next = newNode;
-            cursorMerge=newNode;
+            newNode->next=cursorA;
+            newNode=newNode->next;
             cursorA=cursorA->next;
-            
         }
         else{
-            Node* newNode = new Node();
-            newNode->data= cursorB->data;
-            cursorMerge->next = newNode;
-            cursorMerge=newNode;
+            newNode->next=cursorB;
+            newNode=newNode->next;
             cursorB=cursorB->next;
         }
     }
-    
+
     if( cursorA == NULL){
-        while( cursorB!=NULL){
-            Node* newNode = new Node();
-            newNode->data= cursorB->data;
-            cursorMerge->next = newNode;
-            cursorMerge=newNode;
+        while(cursorB != NULL){
+            newNode->next=cursorB;
+            newNode=newNode->next;
             cursorB=cursorB->next;
         }
+
     }
-    else if ( cursorB == NULL){
-        while(cursorA !=NULL){
-            Node* newNode = new Node();
-            newNode->data= cursorA->data;
-            cursorMerge->next = newNode;
-            cursorMerge=newNode;
+    else if( cursorB == NULL){
+        while(cursorA != NULL){
+            newNode->next=cursorA;
+            newNode=newNode->next;
             cursorA=cursorA->next;
-        }
+        }        
     }
-    mergeNodeHead=mergeNodeHead->next;  //remove head
+    return newHead;
     
-    return mergeNodeHead;
 }
