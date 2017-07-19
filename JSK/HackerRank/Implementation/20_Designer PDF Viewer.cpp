23f13308ae86587e321dd00ecedbf927bf188480
Sweetio <rlawlstjr4876@gmail.com>
2017-07-20 00:53:09 +0900

    Create 20_Designer PDF Viewer.cpp

---
+++ JSK/HackerRank/Implementation/20_Designer PDF Viewer.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    vector<int> h(26);
+    for(int h_i = 0; h_i < 26; h_i++){
+       cin >> h[h_i];
+    }
+    string word;
+    cin >> word;
+    
+    int max = 0;
+    
+    for(int i = 0 ; i < word.size(); i++){
+        if( max < h[word[i] - 97]){
+            max = h[word[i] - 97];
+        }
+    }
+    cout<< max * word.size();
+    return 0;
+}
