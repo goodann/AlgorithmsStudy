773523e33472cef15cf8a0454a9beddd04697075
Sweetio <rlawlstjr4876@gmail.com>
2017-07-20 23:54:48 +0900

    Create 28_Jumping on the Clouds: Revisited.cpp

---
+++ JSK/HackerRank/Implementation/28_Jumping on the Clouds: Revisited.cpp
@@ -0,0 +1,52 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> c(n);
+    for(int c_i = 0;c_i < n;c_i++){
+       cin >> c[c_i];
+    }
+    int energy = 100;
+    int i =0;
+     do{
+         i = (i+k)%n;
+        if( c[i] == 1){
+            energy-=3;
+        }
+        else{
+            energy--;
+        }
+         
+    }while(i != 0);
+    
+    cout<<energy ;
+    
+    
+    return 0;
+}
