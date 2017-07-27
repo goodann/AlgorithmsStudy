8f6c65de65046c0224fe9b377413daa07d5c0e81
Sweetio <rlawlstjr4876@gmail.com>
2017-07-27 20:06:03 +0900

    Create 38_Equalize the Array.cpp

---
+++ JSK/HackerRank/Implementation/38_Equalize the Array.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    vector<int> arr, num(101);
+    int N,a;
+    
+    cin >> N;
+    
+    for(int i = 0 ; i < N; i++){
+        cin >> a;
+        arr.push_back(a);
+        num[a+1]++;
+    }
+    int max=-1;
+    for(int i = 1 ; i < num.size(); i++){
+        if ( num[i] > max){
+            max = num[i];
+        }
+    }
+    
+    cout<< N - max;
+    
+    
+    
+    return 0;
+}
