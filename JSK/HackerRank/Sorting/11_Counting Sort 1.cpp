873fbe334e52397243243158c899bfb36b05b053
Sweetio <rlawlstjr4876@gmail.com>
2017-08-04 12:24:06 +0900

    Create 11_Counting Sort 1.cpp

---
+++ JSK/HackerRank/Sorting/11_Counting Sort 1.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N ,num;
+    cin >> N;
+    vector<int> numbers(100);
+    
+    for(int i = 0 ; i < N ; i++){
+        cin>> num;
+        numbers[num]++;
+    }
+    for(int i = 0 ; i< 100; i++){
+        cout<< numbers[i]<<" ";
+    }
+    
+    
+    
+    return 0;
+}
