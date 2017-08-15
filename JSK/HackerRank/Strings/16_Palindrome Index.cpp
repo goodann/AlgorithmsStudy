d15afdecdfc9ea587686bbf50a3a32b4749f6eef
Sweetio <rlawlstjr4876@gmail.com>
2017-08-15 12:02:05 +0900

    Create 16_Palindrome Index.cpp

---
+++ JSK/HackerRank/Strings/16_Palindrome Index.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int palindromeIndex(string s){
+    // Complete this function
+    for(int i = 0 ; i < s.size()/2;i++){
+        if( s[i] != s[s.size()-i-1]){
+            if(s[i+1] ==  s[s.size()-i-1]){
+                return i;
+            }
+            else if( s[i] == s[s.size()-i-2]){
+                return s.size()-i-1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        string s;
+        cin >> s;
+        int result = palindromeIndex(s);
+        cout << result << endl;
+    }
+    return 0;
+}
